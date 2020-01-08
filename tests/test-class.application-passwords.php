970dc429467de9b28126e8844c28415a471aff19
Kaspars Dambis <hi@kaspars.net>
2020-01-08 21:50:41 +0200

    Test user login

--- tests/test-class.application-passwords.php
+++ tests/test-class.application-passwords.php
@@ -14,4 +14,27 @@ class Test_Application_Passwords extends WP_UnitTestCase {
 	function test_add_hooks() {
 		$this->assertEquals( 10, has_action( 'authenticate', array( 'Application_Passwords', 'authenticate' ) ) );
 	}
+
+	/**
+	 * HTTP Auth headers are used to determine the current user.
+	 *
+	 * @covers Application_Passwords::rest_api_auth_handler()
+	 */
+	public function test_can_login_user_through_http_auth_headers() {
+		$user_id = $this->factory->user->create(
+			array(
+				'user_login' => 'http_auth_login',
+				'user_pass'  => 'http_auth_pass',
+			)
+		);
+
+		$_SERVER['PHP_AUTH_USER'] = 'http_auth_login';
+		$_SERVER['PHP_AUTH_PW']   = 'http_auth_pass';
+
+		$current_user = wp_get_current_user();
+
+		$this->assertEquals( $user_id, $current_user->ID );
+
+		unset( $_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW'] );
+	}
 }
