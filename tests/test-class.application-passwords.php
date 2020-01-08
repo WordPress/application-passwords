dea701c2f44b2e2c17c0764953ab6ee790ea79d7
Kaspars Dambis <hi@kaspars.net>
2020-01-09 00:32:16 +0200

    Regular account passwords shouldn’t be allowed

--- tests/test-class.application-passwords.php
+++ tests/test-class.application-passwords.php
@@ -33,27 +33,36 @@ class Test_Application_Passwords extends WP_UnitTestCase {
 		$user_id = $this->factory->user->create(
 			array(
 				'user_login' => 'http_auth_login',
-				'user_pass'  => 'http_auth_pass',
+				'user_pass'  => 'http_auth_pass', // Shouldn't be allowed for API login.
 			)
 		);
 
+		// Create a new app-only password.
+		$user_app_password = Application_Passwords::create_new_application_password( $user_id, 'phpunit' );
+
+		// Fake a REST API request.
+		add_filter( 'application_password_is_api_request', '__return_true' );
+
+		// Fake an HTTP Auth request with the regular account password first.
 		$_SERVER['PHP_AUTH_USER'] = 'http_auth_login';
 		$_SERVER['PHP_AUTH_PW']   = 'http_auth_pass';
 
 		$this->assertEquals(
 			0,
 			Application_Passwords::rest_api_auth_handler( null ),
-			'None-REST API requests are ignored'
+			'Regular user account password should not be allowed for API authenticaiton'
 		);
 
-		// Fake the REST API request. TODO: how can we mock this or initialize the actual REST API?
-		define( 'REST_REQUEST', true );
+		// Not try with an App password instead.
+		$_SERVER['PHP_AUTH_PW'] = $user_app_password[0];
 
 		$this->assertEquals(
 			$user_id,
 			Application_Passwords::rest_api_auth_handler( null ),
-			'Override the user from HTTP Auth headers during REST API requests'
+			'Application passwords should be allowed for API authentication'
 		);
+
+		remove_filter( 'application_password_is_api_request', '__return_true' );
 
 		// Cleanup all the global state.
 		unset( $_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW'] );
