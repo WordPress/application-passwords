6a38b417b7fcb54f7e788933ce9bb10aaa91ffb2
George Stephanis <georgestephanis@automattic.com>
2020-12-12 15:13:39 -0500

    Shift the classic plugin files into a subdirectory.
    Make a new subdir for the newer implementation assets.  And have the main plugin loader file switch based on the core class being defined.

---
+++ post-5.6/class.application-passwords.php
@@ -0,0 +1,16 @@
+<?php
+
+class Application_Passwords {
+
+	/**
+	 * Add various hooks.
+	 *
+	 * @since 1.0
+	 *
+	 * @access public
+	 * @static
+	 */
+    public static function add_hooks() {
+        // Anything we need to do, add the actions or filters in here.
+    }
+}
